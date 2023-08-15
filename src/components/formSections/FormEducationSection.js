55b2c222c7590ee5646c5f424848e693c0a34fbf
Lakshya Kumar <135580752+its-lakshya@users.noreply.github.com>
2023-08-15 12:00:49 +0530

    Loaded the education form dynamicaly using prevState in useState hook

--- src/components/formSections/FormEducationSection.js
+++ src/components/formSections/FormEducationSection.js
@@ -2,38 +2,44 @@ import { useState } from "react";
 import EducationForm from "./EducationForm";
 
 const FormEducationSection = () => {
-  const [visiblityForm2, setVisiblityForm2] = useState("hidden");
-  const [visiblityForm3, setVisiblityForm3] = useState("hidden");
-  const [buttonVisibility, setButtonVisibility] = useState("visible")
+  // const [visiblityForm2, setVisiblityForm2] = useState("hidden");
+  // const [visiblityForm3, setVisiblityForm3] = useState("hidden");
+  // const [buttonVisibility, setButtonVisibility] = useState("visible");
+  const [Education, setEducation] = useState([<EducationForm/>])
 
   const handleClick = () => {
-    setVisiblityForm2('visible')
-    if(visiblityForm2==='visible'){
-        setVisiblityForm3('visible')
-        setButtonVisibility('hidden')
-    }
-    
-  } 
+    // setVisiblityForm2("visible");
+    // if (visiblityForm2 === "visible") {
+    //   setVisiblityForm3("visible");
+    //   setButtonVisibility("hidden");
+    // }
+    setEducation((prev) => [...prev, <EducationForm/>])
+  };
 
   return (
     <div className="mx-10 py-10 min-h-[83vh] w-1/2 flex items-center">
       <div className="min-h-4/5 w-[95%] flex flex-col justify-center items-start gap-y-12">
-        <div className="font-semibold text-3xl text-black flex flex-col gap-y-3">
+        <div className="font-bold text-4xl text-black flex flex-col gap-y-3">
           Tell us about your education
           <span className="text-lg font-normal">
             Enter your education experience so far, even if you are a current
             student or did not graduate.
           </span>
         </div>
-        <EducationForm />
+        {/* <EducationForm />
         <div className={` ${visiblityForm2} w-full `}>
           <EducationForm />
         </div>
         <div className={` ${visiblityForm3} w-full`}>
           <EducationForm />
-        </div>
-        <button onClick={handleClick} className={` ${buttonVisibility}`}>
-            Add+
+        </div> */}
+        {Education}
+        <button
+          onClick={handleClick}
+          // className={` ${buttonVisibility} text-[#EA4492] -mt-5`}
+          className={`text-[#EA4492] `}
+        >
+          + Add more education
         </button>
       </div>
       <div></div>
