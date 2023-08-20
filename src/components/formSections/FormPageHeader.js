1d5f4c67ed15eee5153a744375733617ff546e2d
Lakshya Kumar <135580752+its-lakshya@users.noreply.github.com>
2023-08-20 14:33:13 +0530

    styled the form page header conditionaly using redux store

--- src/components/formSections/FormPageHeader.js
+++ src/components/formSections/FormPageHeader.js
@@ -1,10 +1,10 @@
 import {
-  PiNumberCircleOneFill,
-  PiNumberCircleTwoFill,
-  PiNumberCircleThreeFill,
-  PiNumberCircleFourFill,
-  PiNumberCircleFiveFill,
-  PiNumberCircleSixFill,
+  PiNumberSquareOneFill,
+  PiNumberSquareTwoFill,
+  PiNumberSquareThreeFill,
+  PiNumberSquareFourFill,
+  PiNumberSquareFiveFill,
+  PiNumberSquareSixFill,
 } from "react-icons/pi";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -19,29 +19,29 @@ const FormPageHeader = () => {
         <Link to="/" className="font-bold text-2xl text-white -mt-2">
           ResumeWisardry
         </Link>
-        <ul className="w-3/5 h-full flex justify-center gap-x-8 font-normal text-lg text-gray-300 [&>*]:flex [&>*]:justify-center [&>*]:items-center [&>*]:gap-x-1 ">
-          <li className={style==='contact' ? 'text-red-400' : null}>
-            <PiNumberCircleOneFill />
+        <ul className="w-3/5 h-full flex justify-center gap-x-8 font-normal text-base text-gray-400 [&>*]:flex [&>*]:justify-center [&>*]:items-center [&>*]:gap-x-1 [&>*]:w-28 ">
+          <li className={style==='contact' ? 'border-b-4 border-b-[#EA4492] bg-white bg-opacity-10 font-semibold -mb-1 text-white [&>*]:text-[#EA4492] [&>*]:text-xl ' : null}>
+            <PiNumberSquareOneFill />
             Contact
           </li>
-          <li className={style==='education' ? 'text-red-400' : null}>
-            <PiNumberCircleTwoFill />
+          <li className={style==='education' ? 'border-b-4 border-b-[#EA4492] bg-white bg-opacity-10 font-semibold -mb-1 text-white [&>*]:text-[#EA4492] [&>*]:text-xl ' : null}>
+            <PiNumberSquareTwoFill />
             Education
           </li>
-          <li className={style==='experience' ? 'text-red-400' : null}>
-            <PiNumberCircleThreeFill />
-            Professionl Experience
+          <li className={style==='experience' ? 'border-b-4 border-b-[#EA4492] bg-white bg-opacity-10 font-semibold -mb-1 text-white [&>*]:text-[#EA4492] [&>*]:text-xl ' : null}>
+            <PiNumberSquareThreeFill />
+            Experience
           </li>
-          <li className={style==='skills' ? 'text-red-400' : null}>
-            <PiNumberCircleFourFill />
+          <li className={style==='skills' ? 'border-b-4 border-b-[#EA4492] bg-white bg-opacity-10 font-semibold -mb-1 text-white [&>*]:text-[#EA4492] [&>*]:text-xl ' : null}>
+            <PiNumberSquareFourFill />
             Skills
           </li>
-          <li className={style==='summary' ? 'text-red-400' : null}>
-            <PiNumberCircleFiveFill />
+          <li className={style==='summary' ? 'border-b-4 border-b-[#EA4492] bg-white bg-opacity-10 font-semibold -mb-1 text-white [&>*]:text-[#EA4492] [&>*]:text-xl ' : null}>
+            <PiNumberSquareFiveFill />
             Summary
           </li>
-          <li className={style==='education' ? 'text-red-400' : null}>
-            <PiNumberCircleSixFill />
+          <li className={style==='finalize' ? 'border-b-4 border-b-[#EA4492] bg-white bg-opacity-10 font-semibold -mb-1 text-white [&>*]:text-[#EA4492] [&>*]:text-xl ' : null}>
+            <PiNumberSquareSixFill />
             Finalize
           </li>
         </ul>
