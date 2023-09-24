181149214daa9678e528007369afda07f92b9a52
Lakshya Kumar <135580752+its-lakshya@users.noreply.github.com>
2023-09-24 19:33:22 +0530

    build the sidebar of template2

--- src/templates/Template2.js
+++ src/templates/Template2.js
@@ -4,8 +4,8 @@ import { BsDot } from "react-icons/bs";
 
 const Template2 = () => {
   const headingCss =
-    "font-extrabold uppercase  tracking-[0.07em] w-full text-sm mb-[2px]";
-  const headingContentCss = "w-full flex flex-col gap-1";
+    "font-bold capitalize   w-full text-base mb-[2px] ";
+  const headingContentCss = "w-full flex flex-col gap-1 ";
 
   const [linksVisibility, setLinkVisibility] = useState("hidden");
   const [certificationsVisibility, setCertificationsVisibility] =
@@ -83,7 +83,7 @@ const Template2 = () => {
 
   return (
     <div id="resume">
-      <div className="self-center h-[222.5mm] w-[160mm] bg-white flex flex-col justify-center items-center ">
+      <div className="self-center h-[222.5mm] w-[180mm] bg-white flex flex-col justify-center items-center ">
         <div
           className={`[&>*]:mx-4 h-44 w-full flex items-center justify-center  bg-[#dad8d9] `}
         >
@@ -111,7 +111,230 @@ const Template2 = () => {
             </div>
           </div>
         </div>
+        <div
+          className={`w-full h-full  flex items-center justify-center ${textColor} text-xs font-semibold  `}
+        >
+          <div className="w-full h-full flex justify-center itmes-center">
+            <div className="py-6 px-[0.8rem] h-full w-80 flex flex-col justify-start items-center gap-y-4  overflow-hidden bg-[#c6c6c6]  ">
+              {styleDetails === "contact" ||
+              contactDetails.name ||
+              contactDetails.phone ? (
+                <div className={`${headingContentCss} text-black`}>
+                  <div className={`${headingCss}`}>contact</div>
+                  <div className="font-semibold text-sm flex items-center gap-x-2 justify-start">
+                    <span>Address : </span>
+                    {contactDetails.city ? (
+                      <span className='text-xs font-light'>{contactDetails.city}, {contactDetails.state}</span>
+                    ) : null}
+                  </div>
+                  <div className="font-semibold text-sm flex items-center gap-x-2">
+                    <span>Pincode : </span>
+                    {contactDetails.country || contactDetails.pincode ? (
+                      <span className='text-xs font-light'>
+                        {contactDetails.pincode}, {contactDetails.country}
+                      </span>
+                    ) : null}
+                  </div>
+                  <div className="font-semibold text-sm flex items-center gap-x-2">
+                    <span>Email : </span>
+                    {contactDetails.email ? (
+                      <span className='text-xs font-light'>{contactDetails.email}</span>
+                    ) : null}
+                  </div>
+                  
+                  <div className="font-semibold text-sm flex items-center gap-x-2">
+                    <span>Phone : </span>
+                    {contactDetails.phone  ? (
+                      <span className='text-xs font-light'>
+                        {contactDetails.phone} 
+                      </span>
+                    ) : null}
+                  </div>
+                </div>
+              ) : null}
+
+              {styleDetails === "education" ||
+              educationDetails.instituteName[0] ? (
+                <div className={`${headingContentCss}`}>
+                  <div className={`${headingCss}`}>Education</div>
+                  <div className="w-full flex flex-col gap-1 mb-2">
+                    <div className="">
+                      {educationDetails.degree[0] ||
+                      educationDetails.fieldofStudy[0] ? (
+                        <span>
+                          {educationDetails.degree[0]} /{" "}
+                          {educationDetails.fieldofStudy[0]}
+                        </span>
+                      ) : null}
+                    </div>
+                    <div className="">
+                      {educationDetails.instituteName[0] ? (
+                        <span>{educationDetails.instituteName[0]}</span>
+                      ) : null}
+                    </div>
+                    <div className="">
+                      {educationDetails.instituteLocation[0] ? (
+                        <span>{educationDetails.instituteLocation[0]}</span>
+                      ) : null}{" "}
+                    </div>
+                    <div className="">
+                      {educationDetails.graduationStart[0] ||
+                      educationDetails.graduationEnd[0] ? (
+                        <span>
+                          {educationDetails.graduationStart[0]} to{" "}
+                          {educationDetails.graduationEnd[0]}
+                        </span>
+                      ) : null}{" "}
+                    </div>
+                  </div>
+                  {educationDetails.instituteName[1] ? (
+                    <div className="w-full flex flex-col gap-1 mb-2">
+                      <div className="">
+                        {educationDetails.degree[1] ||
+                        educationDetails.fieldofStudy[1] ? (
+                          <span>
+                            {educationDetails.degree[1]} /{" "}
+                            {educationDetails.fieldofStudy[1]}
+                          </span>
+                        ) : null}
+                      </div>
+                      <div className="">
+                        {educationDetails.instituteName[1] ? (
+                          <span>{educationDetails.instituteName[1]}</span>
+                        ) : null}
+                      </div>
+                      <div className="">
+                        {educationDetails.instituteLocation[1] ? (
+                          <span>{educationDetails.instituteLocation[1]}</span>
+                        ) : null}{" "}
+                      </div>
+                      <div className="">
+                        {educationDetails.graduationStart[1] ||
+                        educationDetails.graduationEnd[1] ? (
+                          <span>
+                            {educationDetails.graduationStart[1]} to{" "}
+                            {educationDetails.graduationEnd[1]}
+                          </span>
+                        ) : null}{" "}
+                      </div>
+                    </div>
+                  ) : null}
+                  {educationDetails.instituteName[2] ? (
+                    <div className="w-full flex flex-col gap-1 mb-2">
+                      <div className="">
+                        {educationDetails.degree[2] ||
+                        educationDetails.fieldofStudy[2] ? (
+                          <span>
+                            {educationDetails.degree[2]} /{" "}
+                            {educationDetails.fieldofStudy[2]}
+                          </span>
+                        ) : null}
+                      </div>
+                      <div className="">
+                        {educationDetails.instituteName[2] ? (
+                          <span>{educationDetails.instituteName[2]}</span>
+                        ) : null}
+                      </div>
+                      <div className="">
+                        {educationDetails.instituteLocation[2] ? (
+                          <span>{educationDetails.instituteLocation[2]}</span>
+                        ) : null}{" "}
+                      </div>
+                      <div className="">
+                        {educationDetails.graduationStart[2] ||
+                        educationDetails.graduationEnd[2] ? (
+                          <span>
+                            {educationDetails.graduationStart[2]} to{" "}
+                            {educationDetails.graduationEnd[2]}
+                          </span>
+                        ) : null}{" "}
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+              ) : null}
+
+              {styleDetails === "skills" || skillDetails.skill[0] ? (
+                <div className={`${headingContentCss} text-black `}>
+                  <div className={`font-semibold uppercase text-base`}>skills</div>
+                  {skillDetails.skill[0] ? (
+                    <span className="text-sm font-light">{skillDetails.skill[0]}</span>
+                   ) : null} 
+                  {skillDetails.skill[1] ? (
+                    <span className="text-sm font-light">{skillDetails.skill[1]}</span>
+                   ) : null} 
+                  {skillDetails.skill[2] ? (
+                    <span className="text-sm font-light">{skillDetails.skill[2]}</span>
+                   ) : null} 
+                  {skillDetails.skill[3] ? (
+                    <span className="text-sm font-light">{skillDetails.skill[3]}</span>
+                   ) : null} 
+                  {skillDetails.skill[4] ? (
+                    <span className="text-sm font-light">{skillDetails.skill[4]}</span>
+                  ) : null}
+                  {skillDetails.skill[5] ? (
+                    <span className="text-sm font-light">{skillDetails.skill[5]}</span>
+                  ) : null}
+                  {skillDetails.skill[6] ? (
+                    <span className="text-sm font-light">{skillDetails.skill[6]}</span>
+                  ) : null}
+                  {skillDetails.skill[7] ? (
+                    <span className="text-sm font-light">{skillDetails.skill[7]}</span>
+                  ) : null}
+                  {skillDetails.skill[8] ? (
+                    <span className="text-sm font-light">{skillDetails.skill[8]}</span>
+                  ) : null}
+                  {skillDetails.skill[9] ? (
+                    <span className="text-sm font-light">{skillDetails.skill[9]}</span>
+                  ) : null}
+                </div>
+               ) : null} 
+
+              <div className={`${headingContentCss} ${languagesVisibility} text-black`}>
+                <div className={`font-semibold uppercase text-base`}>languages</div>
+                {linkLangDetails.lang1 ? (
+                  <span className="text-sm font-light">{linkLangDetails.lang1}</span>
+                ) : null}
+                {linkLangDetails.lang2 ? (
+                  <span className="text-sm font-light">{linkLangDetails.lang2}</span>
+                ) : null}
+                {linkLangDetails.lang3 ? (
+                  <span className="text-sm font-light">{linkLangDetails.lang3}</span>
+                ) : null}
+                {linkLangDetails.lang4 ? (
+                  <span className="text-sm font-light">{linkLangDetails.lang4}</span>
+                ) : null}
+                {linkLangDetails.lang5 ? (
+                  <span className="text-sm font-light">{linkLangDetails.lang5}</span>
+                ) : null}
               </div>
+
+              <div className={`${headingContentCss} ${linksVisibility}  text-black`}>
+                <div className={`font-semibold uppercase text-base`}>links</div>
+                {linkLangDetails.link1 ? ( 
+                  <span className="text-sm font-light">{linkLangDetails.link1}</span>
+                ) : null}
+                {linkLangDetails.link2 ? (
+                  <span className="text-sm font-light">{linkLangDetails.link2}</span>
+                 ) : null} 
+                 {linkLangDetails.link3 ? (
+                  <span className="text-sm font-light">{linkLangDetails.link3}</span>
+                 ) : null}
+                {linkLangDetails.link4 ? (
+                  <span className="text-sm font-light">{linkLangDetails.link4}</span>
+                 ) : null}
+                {linkLangDetails.link5 ? (
+                  <span className="text-sm font-light">{linkLangDetails.link5}</span>
+                 ) : null}
+              </div>
+
+              
+            </div>
+
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
