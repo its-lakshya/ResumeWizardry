8554c8c9b65afa89c19426d3cb914f0092d1e350
Lakshya Kumar <lakshyakumar@Lakshyas-MacBook-Air.local>
2023-10-21 15:25:58 +0530

    made the build resume landing page responsive

--- src/pages/BuildResumeStartPage.js
+++ src/pages/BuildResumeStartPage.js
@@ -13,32 +13,33 @@ const BuildResumeStartPage = () => {
     <div className="h-screen w-full flex justify-center items-center bg-cover bg-center "
     style={{ backgroundImage: `url(${ResumeCollage})` }}
     >
-      <div className="bg-white h-[35rem] w-[26rem]  flex flex-col justify-center items-center px-12 gap-y-7 shadow-lg shadow-gray-400 ">
-        <div className="text-[#003459] text-3xl font-bold">ResumeWizardry</div>
-        <div className="text-5xl text-center font-bold">
+      <div className="bg-white h-[35rem] w-[26rem] max-lg:w-[20rem] max-lg:h-[28rem] flex flex-col 
+      justify-center items-center px-12 max-lg:px-2 gap-y-7 max-lg:gap-y-2 max-sm:gap-y-4 shadow-lg shadow-gray-400 ">
+        <div className="text-[#003459] text-3xl max-lg:text-2xl max-sm:text-xl font-bold ">ResumeWizardry</div>
+        <div className="text-5xl max-lg:text-3xl max-sm:text-2xl text-center font-bold">
           Just four magical steps
         </div>
         <div className="flex flex-col text-[1.05rem] items-center justify-center gap-y-3">
-          <div className="flex justify-center items-center gap-x-4">
-            <span className="text-4xl text-[#003459]">
+          <div className="flex justify-center items-center gap-x-4 max-lg:gap-x-2">
+            <span className="text-4xl max-lg:text-2xl text-[#003459]">
               <PiNumberCircleOneFill />
             </span>
             Select a template from our library of professional designs
           </div>
-          <div className="flex justify-center items-center gap-x-4">
-            <span className="text-4xl text-[#003459]">
+          <div className="flex justify-center items-center gap-x-4 max-lg:gap-x-2">
+            <span className="text-4xl max-lg:text-2xl text-[#003459]">
               <PiNumberCircleTwoFill />
             </span>
             Build your resume with our industry-specific bullet points
           </div>
-          <div className="flex justify-center items-center gap-x-4">
-            <span className="text-4xl text-[#003459]">
+          <div className="flex justify-center items-center gap-x-4 max-lg:gap-x-2">
+            <span className="text-4xl max-lg:text-2xl text-[#003459]">
               <PiNumberCircleThreeFill />
             </span>
             Customize your resume on your own terms
           </div>
-          <div className="flex justify-center items-center gap-x-4">
-            <span className="text-4xl text-[#003459]">
+          <div className="flex justify-center items-center gap-x-4 max-lg:gap-x-2">
+            <span className="text-4xl max-lg:text-2xl text-[#003459]">
               <PiNumberCircleFourFill />
             </span>
             Download your resume, print it out and get it ready to send!
@@ -51,8 +52,8 @@ const BuildResumeStartPage = () => {
             hoverColor="bg-[#FF9CDA]"
             hoverBorderColor="border-[#FF9CDA]"
             text="build my resume now"
-            height="h-[3.5rem]"
-            width="w-72"
+            height="h-[3.5rem] max-sm:h-[3.2rem]"
+            width="w-72 max-sm:w-56"
           />
         </Link>
       </div>
