8d0105a2d2dfefaa1fb3fb5e4ae25540806be579
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-08-02 21:21:43 +0530

    fix: the name of the website in navbar

--- src/components/homePageSections/Navbar.js
+++ src/components/homePageSections/Navbar.js
@@ -4,12 +4,12 @@ const Navbar = () => {
     return(
         <div className='w-full h-20 font-bold flex justify-center bg-white bg-opacity-95 shadow-lg  '>
             <div className='w-3/4 flex justify-between items-center max-lg:w-full max-lg:px-6'>
-            <span className="font-bold text-3xl text-[#003459] -mt-2 max-sm:text-2xl">ResumeWisardry</span>
+            <span className="font-bold text-3xl text-[#003459] -mt-2 max-sm:text-2xl">ResumeWizardry</span>
             <ul className='flex justify-center gap-x-10 font-medium text-lg text-[#003459] max-sm:hidden'>
                 {/* <li className='hover:text-[#EA4492]'>Resume App</li> */}
-                <li className='hover:text-[#EA4492]'>Resume Tips</li>
+                {/* <li className='hover:text-[#EA4492]'>Resume Tips</li>
                 <li className='hover:text-[#EA4492]'>Templates</li>
-                <li className='hover:text-[#EA4492]'>About</li>
+                <li className='hover:text-[#EA4492]'>About</li> */}
             </ul>
             </div>
         </div>
